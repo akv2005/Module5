c0be2c3a6deadc5d0a7d0d68bfa18243a62273cd
akv05 <akv0571@gmail.com>
2024-05-06 21:12:05 +0900

    Домашняя работа по уроку "Различие атрибутов класса и экземпляра."

--- task3.py
+++ task3.py
@@ -1,24 +1,23 @@
 class Buiding:
    def __init__(self, numberOfFloors, buildingType):
-      self.numberOfFloors = int(numberOfFloors)
-      self.buildingType = str(buildingType)
+      self.numberOfFloors = numberOfFloors
+      self.buildingType = buildingType
 
 
-   def __eq__(self, numberOfFloors, buildingType):
-      return self.numberOfFloors == self.buildingType
+   def __eq__(self, other):
+       return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
 
 
-buiding = Buiding(numberOfFloors=22, buildingType='22')
-buiding1 = Buiding(numberOfFloors=33, buildingType=33)
-print(buiding)
-if buiding.__eq__(buiding.numberOfFloors, type(buiding.buildingType)):
-   print('одинаковы',type(buiding.numberOfFloors), buiding.buildingType)
+
+number_= Buiding(1, '1')
+namber2_ = Buiding(1,'2')
+
+
+
+if Buiding. __eq__(self= number_, other= namber2_):
+    print('одинаковы')
+
 else:
-   print('не одинаковы', type(buiding.buildingType))
-   print(type(buiding.numberOfFloors), buiding.numberOfFloors, type(buiding.buildingType), buiding.buildingType)
+   print('не одинаковы')
 
-if buiding.__eq__(buiding1.numberOfFloors, type(buiding1.buildingType)):
-   print('одинаковы',type(buiding1.numberOfFloors), buiding1.buildingType)
-else:
-   print('не одинаковы', type(buiding1.buildingType))
-   print(type(buiding1.numberOfFloors), buiding1.numberOfFloors, type(buiding1.buildingType), buiding1.buildingType)
+
